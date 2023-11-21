d665ced8ab8aca9a65094d9f192da038cfa943bb
Mart624i <martinahlmanpedersen@gmail.com>
2023-11-21 14:31:28 +0100

    Basic enemy functionality

---
+++ 2DGame/Assets/Scripts/Mobs/Interfaces/IProjectile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.AI
+{
+    public interface IProjectile
+    {
+        void Start();
+        void FixedUpdate();
+        Vector3 FindTargetPosition();
+        void ShootTargetPosition();
+    }
+}
