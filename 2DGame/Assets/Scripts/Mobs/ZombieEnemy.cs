c99b2a324e412d32765d7f8c58649554a0e01521
Mart624i <martinahlmanpedersen@gmail.com>
2023-12-17 10:24:03 +0100

    Zombie prefab

--- 2DGame/Assets/Scripts/Mobs/ZombieEnemy.cs
+++ 2DGame/Assets/Scripts/Mobs/ZombieEnemy.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts.AI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,7 +13,7 @@ public class ZombieEnemy : MonoBehaviour, IEnemy
 {
     public int health;
     public float movementSpeed;
-    private float detectionRadius;
+    public float detectionRadius;
 
     // Attack
     public float damage;
@@ -20,39 +21,53 @@ public class ZombieEnemy : MonoBehaviour, IEnemy
     public float attackSpeed;
     public float chargeSpeed;
 
-    public GameObject lootPrefab;
-    public int lootDropChange;
+    //public GameObject lootPrefab;
+    //public int lootDropChange;
+
+    Vector2 movement;
 
     public Transform _target;
-    private Animator _animator;
-    private Rigidbody2D _rigidbody;
+    public Animator _animator;
+    public Rigidbody2D _rigidbody;
+
 
     public void Start()
     {
+        Debug.Log("Starting");
         setTarget(GameObject.FindGameObjectWithTag("Player").transform);
     }
 
     public void FixedUpdate()
     {
-        _animator.SetTrigger("IdleTrigger");
+        // _animator.SetTrigger("IdleTrigger");
         float distanceToPlayer = Vector2.Distance(transform.position, _target.position);
         if (distanceToPlayer < detectionRadius)
         {
+            
             if (distanceToPlayer > attackRange)
             {
                 MoveTowardsTarget(_target);
+                
             }
             else
             {
                 Attack();
+                Debug.Log("Attacking");
             }
         }
     }
 
     public void MoveTowardsTarget(Transform target)
     {
+        Vector2 direction = _target.position - transform.position;
+        direction.Normalize();
+
         _animator.SetTrigger("WalkTrigger");
-        transform.position = Vector2.MoveTowards(transform.position, target.position, movementSpeed * Time.deltaTime);
+        // transform.localPosition = Vector2.MoveTowards(transform.position, target.position, movementSpeed * Time.deltaTime);
+        //transform.localPosition = _rigidbody.MovePosition(_rigidbody.position + movement.normalized * movementSpeed * Time.fixedDeltaTime);
+        _rigidbody.velocity = direction * movementSpeed;
+        Debug.Log("Did it work?");
+        
     }
 
     public void ChargeTowardsTarget(Transform target)
@@ -93,18 +108,18 @@ public class ZombieEnemy : MonoBehaviour, IEnemy
         if(_animator != null)
         {
             _animator.SetTrigger("DieTrigger");
-            DropLoot();
+            //DropLoot();
             Destroy(gameObject);
         }
     }
 
-    public void DropLoot()
-    {
-        if (Random.Range(0, 100) < lootDropChange && lootPrefab != null)
-        {
-            Instantiate(lootPrefab, transform.position, Quaternion.identity);
-        }
-    }
+    //public void DropLoot()
+    //{
+    //    if (UnityEngine.Random.Range(0, 100) < lootDropChange && lootPrefab != null)
+    //    {
+    //        Instantiate(lootPrefab, transform.position, Quaternion.identity);
+    //    }
+    //}
 
     // Setters
     public ZombieEnemy setTarget(Transform target)
