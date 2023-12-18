765a4cf296ea70bd255f0d1a49485cb75783ec1a
Martin A. Pedersen <martinahlmanpedersen@gmail.com>
2023-12-18 13:45:39 +0100

    AI movement and animations working

---
+++ 2DGame/Assets/Scripts/Mobs/Interfaces/IEnemyMovement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.AI
+{
+    public interface IEnemyMovement
+    {
+        void MoveTowardsPlayer(Transform player);
+        void BeIdle();
+    }
+}
